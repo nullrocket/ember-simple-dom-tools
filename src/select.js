e5fb4c6435f28f0da9fa02a73ffe7dba769182c9
nullrocket <doug.hill@passpack.com>
2017-07-31 02:55:14 -0700

    Working on jsdoc to markdown generator

--- src/select.js
+++ src/select.js
@@ -2,26 +2,64 @@ let idMatch = /^#[\w-]*$/,
   classMatch = /^\.[\w-]*$/,
   htmlMatch = /<.+>/,
   singlet = /^\w+$/;
-let isString = function(item) { return typeof item === typeof ''; };
+let isString = function ( item ) { return typeof item === typeof ''; };
+
+
+/*elementList = document.querySelectorAll(selectors);
+where
+
+elementList is a non-live NodeList of element objects.
+selectors is a string containing one or more CSS selectors separated by commas.
+*/
+
+
+/*
+element = document.getElementById(id);
+Parameters
+id
+is a case-sensitive string representing the unique ID of the element being sought.
+  Return Value
+element
+is a reference to an Element object, or null if an element with the specified ID is not in the document.
+*/
+
+
+/*
+var elements = document.getElementsByClassName(names); // or:
+var elements = rootElement.getElementsByClassName(names);
+elements is a live HTMLCollection of found elements.
+names is a string representing the list of class names to match; class names are separated by whitespace
+getElementsByClassName can be called on any element, not only on the document. The element on which it is called will be used as the root of the search.
+ */
+
+
+/*
+var elements = document.getElementsByTagName(name);
+elements is a live HTMLCollection (but see the note below) of found elements in the order they appear in the tree.
+name is a string representing the name of the elements. The special string "*" represents all elements.
+The latest W3C specification says elements is an HTMLCollection; however, this method returns a NodeList in WebKit browsers. See bug 14869 for details.
+ */
 function find( selector, context ) {
   context = context || document;
   let elems;
+  console.log(selector/*,context.querySelectorAll(selector.replace(/([.:+~#> ])/g,"\$1") )*/);
   try {
-     elems = (
+    elems = (
       classMatch.test(selector) ?
-        context.getElementsByClassName(selector.slice(1)) :
+        context.getElementsByClassName(selector.slice(1))
+        :
         singlet.test(selector) ?
-          context.getElementsByTagName(selector) :
+          context.getElementsByTagName(selector)
+          :
           context.querySelectorAll(selector)
     );
   }
-  catch(e){
+  catch ( e ) {
     return [];
   }
+  console.log(elems);
   return elems;
 }
-
-
 
 
 /**
@@ -29,33 +67,26 @@ function find( selector, context ) {
  * @function select
  * @param {string} selector A valid css selector
  * @param {DomElement} context A DOM element to use as context
- * @returns {Array}
+ * @returns {Array} Returns an array of elements matching the selector, or an empty array if no elements match.
  */
 export default function ( selector, context ) {
 
   if ( !selector ) { return []}
   let elems;
   if ( isString(selector) ) {
-    elems = (
-      idMatch.test(selector) ?
-        // If an ID use the faster getElementById check
-        document.getElementById(selector.slice(1)) :
+    if ( idMatch.test(selector) ) {
+      // If an ID use the faster getElementById check
+      let found = document.getElementById(selector.slice(1));
 
-          find(selector,context)
-    );
+      return found ? [found] : []
 
-    // If function, use as shortcut for DOM ready
-  }
-  if(!elems)
-  {
-    return [];
-  }
-  if ( elems.nodeType || elems === window ) {
-    return [elems];
+    }
+    else {
 
-  } else {
-    // Treat like an array and loop through each item.
-    return elems;
+     return  find(selector, context)
+    }
+
   }
+  return elems ? elems : [];
 
 }
--- tests/unit/select-test.js
+++ tests/unit/select-test.js
@@ -1,7 +1,73 @@
 
 import { test  } from 'ember-qunit';
-import {select} from 'ember-simple-dom-tools';
+import {select,create,append} from 'ember-simple-dom-tools';
 
-test("it exists", function(assert){
+test("select function exists", function(assert){
   assert.ok(select);
+
+
+});
+
+test("ID Selection",function(assert){
+  assert.expect(14);
+  let fixture = document.getElementById('ember-testing');
+
+
+  assert.ok(select('#ember-testing')[0] instanceof HTMLDivElement,"Selects a single element");
+  append(create('<ul id="first"></ul>'),fixture);
+  assert.ok(select('ul#first')[0] instanceof HTMLUListElement,"ID Selector w/ Element");
+  append(create('<div id="firstp"><div id="simon1"></div></div>'),fixture);
+  assert.equal(select('#firstp #simon1').length,1, "ID selector with existing ID descendant");
+  assert.equal(select('#firstp #foobar').length,0, "ID selector with non-existent ID descendant");
+  append(create('<form id="台北Táiběi"><div id="台北"></div></form>'),fixture);
+  assert.equal(select('#台北Táiběi').length,1, "ID selector using UTF8");
+
+  assert.equal(select('#台北Táiběi, #台北').length,2, "ID selector using UTF8");
+  assert.equal(select('div #台北').length,1, "Descendant ID selector using UTF8");
+  assert.equal(select('form > #台北').length,1, "Child ID selector using UTF8");
+  append(create('<form><div id="foo:bar"></div></form>'),fixture);
+  assert.equal(select('#foo\\:bar').length,1, "Escaped ID Selector");
+  append(create('<form><div id="test.foo[5]bar"></div></form>'),fixture);
+  assert.equal(select('#test\\.foo\\[5\\]bar').length,1, "Escaped ID Selector");
+  assert.equal(select('div #foo\\:bar').length,1, "Descendant Escaped ID Selector");
+  assert.equal(select('div #test\\.foo\\[5\\]bar').length,1, "Escaped ID Selector");
+  assert.equal(select('form > #foo\\:bar').length,1, "Escaped ID Selector");
+  assert.equal(select('form > #test\\.foo\\[5\\]bar').length,1, "Escaped ID Selector");
+  /*
+
+
+ // assert.t( "ID Selector w/ Element", "ul#first", [] );
+  //assert.t( "ID selector with existing ID descendant", "#firstp #simon1", [ "simon1" ] );
+  //assert.t( "ID selector with non-existent descendant", "#firstp #foobar", [] );
+  //assert.t( "ID selector using UTF8", "#台北Táiběi", [ "台北Táiběi" ] );
+  //assert.t( "Multiple ID selectors using UTF8", "#台北Táiběi, #台北", [ "台北Táiběi", "台北" ] );
+  //assert.t( "Descendant ID selector using UTF8", "div #台北", [ "台北" ] );
+ // assert.t( "Child ID selector using UTF8", "form > #台北", [ "台北" ] );
+
+ // assert.t( "Escaped ID", "#foo\\:bar", [ "foo:bar" ] );
+  //assert.t( "Escaped ID", "#test\\.foo\\[5\\]bar", [ "test.foo[5]bar" ] );
+  //assert.t( "Descendant escaped ID", "div #foo\\:bar", [ "foo:bar" ] );
+  //assert.t( "Descendant escaped ID", "div #test\\.foo\\[5\\]bar", [ "test.foo[5]bar" ] );
+ // assert.t( "Child escaped ID", "form > #foo\\:bar", [ "foo:bar" ] );
+  //assert.t( "Child escaped ID", "form > #test\\.foo\\[5\\]bar", [ "test.foo[5]bar" ] );
+
+  assert.t( "ID Selector, child ID present", "#form > #radio1", [ "radio1" ] ); // bug #267
+  assert.t( "ID Selector, not an ancestor ID", "#form #first", [] );
+  assert.t( "ID Selector, not a child ID", "#form > #option1a", [] );
+
+  assert.t( "All Children of ID", "#foo > *", [ "sndp", "en", "sap" ] );
+  assert.t( "All Children of ID with no children", "#firstUL > *", [] );
+
+  a = jQuery( "<a id='backslash\\foo'></a>" ).appendTo( "#qunit-fixture" );
+  assert.t( "ID Selector contains backslash", "#backslash\\\\foo", [ "backslash\\foo" ] );
+
+  assert.t( "ID Selector on Form with an input that has a name of 'id'", "#lengthtest", [ "lengthtest" ] );
+
+  assert.t( "ID selector with non-existent ancestor", "#asdfasdf #foobar", [] ); // bug #986
+
+  assert.t( "Underscore ID", "#types_all", [ "types_all" ] );
+  assert.t( "Dash ID", "#qunit-fixture", [ "qunit-fixture" ] );
+
+  assert.t( "ID with weird characters in it", "#name\\+value", [ "name+value" ] );
+*/
 });
