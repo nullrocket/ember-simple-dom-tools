40a1d7bbc961e0835ff73dd8bede33108189c828
nullrocket <doug.hill@passpack.com>
2017-08-01 05:09:38 -0700

    select by ID tests

---
+++ tests/unit/css-test.js
@@ -0,0 +1,25 @@
+
+import { test  } from 'ember-qunit';
+import {select,empty,create,append} from 'ember-simple-dom-tools';
+import {css} from 'ember-simple-dom-tools/css';
+import {style} from 'ember-simple-dom-tools/css';
+
+test("width function exists", function(assert){
+  assert.ok(css);
+});
+
+
+test("Get widths", function ( assert ) {
+
+  empty(document.getElementById('ember-testing'));
+  let fixture = document.getElementById('ember-testing');
+  let testDom = `
+  <div id="first" style="width:200px;height:200px"></div>`;
+  append(create(testDom), fixture);
+  style(select('#first')[0],'background','blue');
+
+  assert.equal(style(select('#first')[0],'background'),'blue', "Get width of single element");
+
+
+
+});
