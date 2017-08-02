52cd002de2163e059c5122178dbae69ad222a559
nullrocket <doug.hill@passpack.com>
2017-08-02 04:18:25 -0700

    select by ID tests

---
+++ config/ember-try.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "scenarios": [
+  {
+    "name": "default",
+    "bower": {
+      "dependencies": {}
+    }
+  },
+  {
+    "name": "ember-release",
+    "bower": {
+      "dependencies": {
+        "ember": "release"
+      }
+    }
+  },
+  {
+    "name": "ember-beta",
+    "bower": {
+      "dependencies": {
+        "ember": "beta"
+      }
+    }
+  },
+  {
+    "name": "ember-canary",
+    "bower": {
+      "dependencies": {
+        "ember": "canary"
+      }
+    }
+  }
+]
+
+};
