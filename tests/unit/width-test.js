a432f2f096a2dc5fbda2e14722f0e9140baedcfa
nullrocket <doug.hill@passpack.com>
2017-08-14 02:35:41 -0700

    Update docs

--- tests/unit/width-test.js
+++ tests/unit/width-test.js
@@ -19,30 +19,31 @@ test('Width test docs',function(assert){
 
   let contentDom = select('.content',fixture);
   let paragraphDom = select('p',fixture);
-  assert.strictEqual(width(contentDom),500, "Get width of single element");
-  assert.deepEqual(width(contentDom,DIMENSION_OPTIONS.RETURN_ARRAY),[ 500 ], "Get width of single element as an array");
 
-  assert.strictEqual(width(paragraphDom), 500, "Get width of multiple elements, return first value only");
-  assert.deepEqual(width(paragraphDom, DIMENSION_OPTIONS.RETURN_ARRAY), [500,500,500], "Get width of multiple elements as an array");
-  assert.strictEqual(width(contentDom,600), 600, "Set width of a single element");
-  assert.deepEqual(width(contentDom,700,DIMENSION_OPTIONS.RETURN_ARRAY), [700], "Set width of a single element return an array");
-  assert.deepEqual(width(paragraphDom,700), 700, "Set width of an array of elements, return single value");
-  assert.deepEqual(width(paragraphDom,800,DIMENSION_OPTIONS.RETURN_ARRAY), [800,800,800], "Set width of an array of elements, return array");
+  var widthValue = width(contentDom);
+  assert.strictEqual(widthValue,500, "Get width of single element");
 
+  widthValue = width(contentDom,DIMENSION_OPTIONS.RETURN_ARRAY);
+  assert.deepEqual(widthValue,[ 500 ], "Get width of single element as an array");
 
+  widthValue = width(paragraphDom);
+  assert.strictEqual(widthValue, 500, "Get width of multiple elements, return first value only");
 
-});
-test("Get widths", function ( assert ) {
+  widthValue = width(paragraphDom, DIMENSION_OPTIONS.RETURN_ARRAY);
+  assert.deepEqual( widthValue, [500,500,500], "Get width of multiple elements as an array");
 
-  empty(document.getElementById('ember-testing'));
-  let fixture = document.getElementById('ember-testing');
-  let testDom = `
-  <div id="first" style="width:200px"></div>
-`;
-  append(create(testDom), fixture);
-    //console.log('select',select('#first'));
- // console.log(width(select('#first')));
-  assert.strictEqual(width(select('#first')),200, "Get width of single element");
+  widthValue = width(contentDom,600);
+  assert.strictEqual(widthValue, 600, "Set width of a single element");
+
+  widthValue = width(contentDom,700,DIMENSION_OPTIONS.RETURN_ARRAY);
+  assert.deepEqual(widthValue, [700], "Set width of a single element return an array");
+
+  widthValue = width(paragraphDom,700);
+  assert.deepEqual(widthValue, 700, "Set width of an array of elements, return single value");
+
+  widthValue = width(paragraphDom,800,DIMENSION_OPTIONS.RETURN_ARRAY);
+  assert.deepEqual(widthValue, [800,800,800], "Set width of an array of elements, return array");
+
 
 
 });
